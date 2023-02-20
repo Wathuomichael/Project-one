8acf4d1009be381aab3c7233299000117a2947ab
Wathuo <wathuomichael@gmail.com>
2023-02-20 22:35:10 +0300

    path fix

--- server.js
+++ server.js
@@ -56,7 +56,7 @@ app.delete('/:id', async(req, res) => {
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
 });
 
 app.listen(PORT, (req, res) => {
