3a7304e02c069c98cb50c552fcd90dfe696ae3db
Wathuomichael <wathuomichael@gmail.com>
2024-05-06 09:11:40 +0300

    app and greeting

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -53,7 +53,7 @@ function App() {
 
   if(isLoading) {
     return (
-      <div class="lds-ripple"><div></div><div></div></div>
+      <div className="lds-ripple"><div></div><div></div></div>
     );
   } else if(isAdded) {
     return (
